e5078db4f41a82da8977046b6e360a67bbfbb230
smaldos64 <gudumholm34@gmail.com>
2023-03-07 13:11:05 +0100

    Nu virker det med ScottPlot. Lille detalje omkring manglende tekst på de 2 pile i plottet skal lige fikses.

--- MatematikProgram/MainWindow.xaml.cs
+++ MatematikProgram/MainWindow.xaml.cs
@@ -20,6 +20,10 @@ using MatematikProgram.Constants;
 using MatematikProgram.Models;
 
 using InteractiveDataDisplay;
+using ScottPlot;
+using ScottPlot.WPF;
+using System.Diagnostics.Metrics;
+using System.Reflection;
 
 namespace MatematikProgram
 {
@@ -126,24 +130,32 @@ namespace MatematikProgram
         {
             VaekstFormelClass_Object.ClearVariables();
             VaekstformelBeregningerDataGrid.Visibility = Visibility.Hidden;
-            //linegraph.Visibility = Visibility.Hidden;
+            PlotPanelVaekstFormel.Visibility = Visibility.Hidden;
         }
 
         private void BeregnOgVisDataGrid(VaekstFormelClass VaekstFormelClass_Object)
         {
             List<VaekstFormelClass> VaekstFormelClassList = new List<VaekstFormelClass>();
             double StartKapital = VaekstFormelClass_Object.StartKapital;
+            double[] dataXaxis = new double[VaekstFormelClass_Object.AntalTerminer];
+            double[] dataYaxis = new double[VaekstFormelClass_Object.AntalTerminer];
+
+            dataXaxis[0] = 0;
+            dataYaxis[0] = StartKapital;
 
             for (int Counter = 0; Counter < VaekstFormelClass_Object.AntalTerminer; Counter++)
             {
                 VaekstFormelClass VaekstFormelClass_Object_Work = new VaekstFormelClass();
-
+                
                 VaekstFormelClass_Object_Work.AntalTerminer = Counter + 1;
                 VaekstFormelClass_Object_Work.Rentesats = VaekstFormelClass_Object.Rentesats;
                 VaekstFormelClass_Object_Work.StartKapital = Math.Round(StartKapital, Const.DefaultNumberOfDecimals);
                 VaekstFormelClass_Object_Work.SlutKapital = Math.Round(StartKapital * (1 + VaekstFormelClass_Object.Rentesats / 100), Const.DefaultNumberOfDecimals);
                 
                 VaekstFormelClassList.Add(VaekstFormelClass_Object_Work);
+
+                dataXaxis[Counter] = Counter + 1;
+                dataYaxis[Counter] = VaekstFormelClass_Object_Work.SlutKapital;
 
                 StartKapital = VaekstFormelClass_Object_Work.SlutKapital;
 
@@ -155,11 +167,34 @@ namespace MatematikProgram
             VaekstformelBeregningerDataGrid.ItemsSource = VaekstFormelClassList;
             VaekstformelBeregningerDataGrid.Visibility = Visibility.Visible;
 
-            var x = Enumerable.Range(0, 1001).Select(i => i / 10.0).ToArray();
-            var y = x.Select(v => Math.Abs(v) < 1e-10 ? 1 : (Math.Sin(v) / v)*10).ToArray();
+            WpfPlot1.Plot.Clear();
+            WpfPlot1.Plot.XAxis.Label("Antal Terminer");
+            WpfPlot1.Plot.YAxis.Label("Indestående (kr)");
+            WpfPlot1.Plot.XAxis2.Label("Udvikling af Beløb");
+            
+            var Arrow1 = WpfPlot1.Plot.AddArrow(dataXaxis[0], dataYaxis[0], dataXaxis[0], dataYaxis[0] + 100,
+                                   lineWidth: 1, color: System.Drawing.Color.Blue);
+            Arrow1.ArrowheadLength = 5;
+            Arrow1.ArrowheadWidth = 2;
+            Arrow1.MinimumLengthPixels = 25;
+            Arrow1.Label = "Start Beløb";
+            
+            var Arrow2 = WpfPlot1.Plot.AddArrow(dataXaxis[VaekstFormelClass_Object.AntalTerminer - 1],
+                                                VaekstFormelClass_Object.SlutKapital, 
+                                                dataXaxis[VaekstFormelClass_Object.AntalTerminer - 1],
+                                                VaekstFormelClass_Object.SlutKapital + 100,
+                                                lineWidth: 1, 
+                                                color: System.Drawing.Color.Green);
+            Arrow2.ArrowheadLength = 5;
+            Arrow2.ArrowheadWidth = 2;
+            Arrow2.MinimumLengthPixels = 25;
+            Arrow2.Label = "Slut Beløb";
 
-            linegraph.Plot(x, y); // x and y are IEnumerable<double>
-            //linegraph.Visibility = Visibility.Visible;
+            WpfPlot1.Plot.AddScatter(dataXaxis, dataYaxis);
+            WpfPlot1.Plot.SaveFig("Beregning.png");
+            WpfPlot1.Refresh();
+
+            PlotPanelVaekstFormel.Visibility = Visibility.Visible;
         }
 
 
